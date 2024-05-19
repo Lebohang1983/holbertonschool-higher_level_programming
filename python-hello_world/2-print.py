11863a9541fd3f5ddbfdafe16fd78a588b3021a7
Lebohang1983 <ntjalebohang@yahoo.com>
2024-05-19 02:12:57 +0000

    python

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_message():
-    print('"Programming is like building a multilingual puzzle\n')
+    print('"Programming is like building a multilingual puzzle')
 # Call the function to print the message
 print_message()
