d3a6323eecbb2f5e8f6401ead6f66c2ba48d9c9d
Lebohang1983 <ntjalebohang@yahoo.com>
2024-07-04 02:51:56 +0000

    lower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-def is_lower(c):
+def islower(c):
     if ord('a') <= ord(c) <= ord('z'):
         return True
     else:
         return False
-print(is_lower('a'))
-print(is_lower('Z'))
+
+print(islower('a'))
+print(islower('Z'))
+print(islower('m'))
