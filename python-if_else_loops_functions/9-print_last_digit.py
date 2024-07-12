7b76f6eeb74b60a83257cb660b97b935dee5f7a2
Lebohang1983 <ntjalebohang@yahoo.com>
2024-07-12 13:27:19 +0000

    last

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    number == (-100000, 1000000)
-    last_digit(number) == number % 10
-    print(f"{last_digit(number)}")
+    last_digit = abs(number) % 10
+    print(last_digit)
+    return last_digit
        
