3cec2abdf47c34159b006c04ce997353f4c460b6
Lebohang1983 <ntjalebohang@yahoo.com>
2024-07-15 15:38:38 +0000

    Power

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for range(1, 101):
+    if {} % 3 == 0
+        print("Fizz")
+    if {} % 5 == 0
+        print("Buzz")
+    elif {} % 3 == 0 && {} % 5 == 0
+        print("FizzBuzz") 
+    else
+        print("{}{}" end= " ")
