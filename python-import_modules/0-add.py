1040240b54677bae5c88031aebe434d698ee9373
Lebohang1983 <ntjalebohang@yahoo.com>
2024-07-22 16:45:36 +0000

    modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-result = a + b
-return result
+def add(a, b):
+    a = 1
+    b = 2
+    result = a + b
+    return result
+
+if__name__ == "__main__":
+    result = add(a, )
+    print(result)
