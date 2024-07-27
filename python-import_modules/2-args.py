c198adf1c082b74fe3b045095e23a5022cab1c6c
Lebohang@1983 <ntjalebohang@yahoo.com>
2024-07-27 14:48:58 +0000

    arg

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    number = len(sys.argv) - 1
+    if number == 1:
+        print(f"{number} argument")
+    elif number > 1:
+        print(f"{number} arguments:")
+    else:
+        print(f"{number} arguments.")
