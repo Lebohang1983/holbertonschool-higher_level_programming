c60b485b007bdacdd276c2026e90d43b948365bd
Lebohang1983 <ntjalebohang@yahoo.com>
2024-07-29 06:06:46 +0000

    sum

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+def add_all(*args):
+    return sum(args)
+    number = len(sys.argv) - 1
+    if number = int():
+        result = add_all(number)
+        print(result)
