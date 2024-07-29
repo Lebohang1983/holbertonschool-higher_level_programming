f1fc9ac289a163fc4eaaafd27402c63afabfd529
Lebohang1983 <ntjalebohang@yahoo.com>
2024-07-29 06:27:41 +0000

    sum

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
+
+
 def add_all(*args):
     return sum(args)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     numbers = map(int, sys.argv[1:])
     result = add_all(*numbers)
     print(result)
