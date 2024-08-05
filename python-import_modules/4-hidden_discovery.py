b9f97882977e7a8fa8a71354da98069719383acc
Lebohang1983 <ntjalebohang@yahoo.com>
2024-08-05 12:07:05 +0000

    names

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
-import sys
-import importlib.util
-import os
-
-def main():
-    module_path = "/tmp/hidden_4.pyc"
-
-
-    spec = importlib.util.spec_from_file_location("hidden_4", module_path)
-    hidden_4 = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(hidden_4)
-
-
-
-    names = dir(hidden_4)
-
-
-
-    names = [name for name in names if not name.startswith("__")]
-
-
-    names.sort()
-
-
-    for name in names:
-        print(name)
-
-
 if __name__ == "__main__":
-    main()
+    import hidden_4 as mod
+
+
+    for prop in dir(mod):
+        if prop[:2] != "__":
+            print(prop)
